ebcdeec27979b7dabb8af2b52d3e7e1b409a1f39
durkafee <mcafee.burke@gmail.com>
2026-02-09 17:19:34 -0600

    Add Family Fitness app source files and configuration

---
+++ FamilyFitnessApp.swift
@@ -0,0 +1,12 @@
+// Swift file containing the app entry point
+
+import SwiftUI
+
+@main
+struct FamilyFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
